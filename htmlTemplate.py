edef77b125b617edec874b893fdd1e9e756317cf
Rashmi Shree Veeraiah <rashmi.v238@gmail.com>
2024-04-02 11:34:44 -0700

    Initial commit

---
+++ htmlTemplate.py
@@ -0,0 +1,39 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #313638
+}
+.chat-message.bot {
+    background-color: #FA6625
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="message">{{MSG}}</div>
+</div>
+
+'''
+
+user_template = '''
+<div class="chat-message user">
+     <div class="message">{{MSG}}</div>
+</div>
+'''
